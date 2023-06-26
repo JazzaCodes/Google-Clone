2e08510f3e8133c1ed4b4479c6b4ac0486432b78
Jazmyn <116229166+JazzaCodes@users.noreply.github.com>
2023-06-26 18:30:49 -0400

    Added files via upload
    Added files

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('myButton')
+document.getElementById('luckyButton')
+
+const button = document.getElementById('myButton');
+button.addEventListener('click' , function() {
+    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley';
+} );
+
+const luckButton = document.getElementById('luckyButton');
+luckyButton.addEventListener('click', function() {
+    window.location.href = 'https://www.youtube.com/watch?v=CCHdMIEGaaM&ab_channel=convarHUN'
+} );
